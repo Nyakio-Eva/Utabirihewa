e4b53b6a1a3d2f297af8994f8df9c764622b6a92
Nyakio-Eva <evalkariuki@gmail.com>
2025-12-01 17:59:06 +0300

    edit argv to integer

--- app.php
+++ app.php
@@ -2,24 +2,27 @@
 
 require_once __DIR__ . '/vendor/autoload.php';  //makes sure we have access to all the files in src directory
 
-$dotenv=Dotenv\Dotenv :: createImmutable(__DIR__);
-$dotenv -> load();
+$dotenv=Dotenv\Dotenv :: createImmutable(__DIR__);  //create a dotenv loader object to load env file from root directory, ensure no overwriing of existing variables
 
-use Evie\Weatherapp\WeatherService;
+$dotenv -> load(); //read the env file
 
-$weatherService= new WeatherService();
+use Evie\Weatherapp\WeatherService; //import the weather service class
 
-if ($argc < 2){
+$weatherService= new WeatherService(); //create an instance of weatherservice
+
+if ($argc < 2){  //argc is the argument count in argv ,its an integer. this checks if the number of arguments provided is 2
     echo "use php app.php city name! \n";
-    exit(1);
+    exit(1); //stops the script. exit(1)=program ended with an error, exit(0)= script program stopped successfully
 }
 
-$city=$argv['1'];
+$city=$argv[1]; //argv is the array containing arguments passed to your php script in the cli . var city extracts the second argument in the array which is the city name
+//argv is a numerical array, so its best to use integers
 
 echo "Get weather for $city city\n";
 
-$weather = $weatherService->getWeather($city);
+$weather = $weatherService->getWeather($city); //call the getweather method and store the data in the weather variable
 
+//display the data in the cli
 echo"\n";
 echo "The temperature is:" . $weather['temperature'] . " Celsius\n";
 echo "The humidity is:" . $weather['humidity']. " %\n";
